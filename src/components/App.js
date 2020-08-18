08e2c43ec86cf35e8d8761ac988c5df65e1ff1be
Luzaks <kanemekanik@gmail.com>
2020-08-18 13:55:24 -0500

    Feat: Add the configuration for the skew animation.

--- src/components/App.js
+++ src/components/App.js
@@ -10,9 +10,30 @@ function App() {
   const app = useRef();
   const scrollContainer = useRef();
 
+  const skewConfigs = {
+    ease: 1,
+    current: 0,
+    previous: 0,
+    rounded: 0
+  }
+
   useEffect(() => {
     document.body.style.height = `${scrollContainer.current.getBoundingClientRect().height}px`;
   }, [size.height]);
+
+  const skewScrolling = () => {
+    skewConfigs.current = window.scrollY;
+    skewConfigs.previous += (skewConfigs.current - skewConfigs.previous) * skewConfigs.ease;
+    skewConfigs.rounded = Math.round(skewConfigs.previous * 100) / 100;
+
+    const difference = skewConfigs.current - skewConfigs.rounded;
+    const acceleration = difference / size.width;
+    const velocity = +acceleration;
+    const skew = velocity * 7.5;
+
+    scrollContainer.current.style.transform = `translate3d(0, -${skewConfigs.rounded}px, 0) skewY(${skew})deg`;
+
+  };
 
   return (
     <div ref={app} className='App'>
