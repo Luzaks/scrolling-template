599493e9b4e5fb6e3ddb9fe227a123b3ef4f2e98
Luzaks <kanemekanik@gmail.com>
2020-08-18 13:05:13 -0500

    Feat: Add configuration for app component.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import images from './images/images';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='App'>
+        <div className='scroll'>
+          {images.map((image, index) => (
+              <>
+                <div key={index} className='img-container'>
+                  <img src={image} alt={`people ${index}`} />
+                </div>
+                <h2>
+                  Skew <span className='outline'>Scrolling</span>
+                </h2>
+              </>
+          ))}
+        </div>
+      </div>
   );
 }
 
