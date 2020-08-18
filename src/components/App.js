c8fab6229dc97c0ad7dff709f5789df008f4298d
Luzaks <kanemekanik@gmail.com>
2020-08-18 13:44:50 -0500

    Feat: Add useWindow custom hooks.

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,17 @@ import React, { useRef, useEffect } from 'react';
 import '../styles/App.css';
 import imagesHelper from '../helpers/imagesHelper';
 import images from '../images/images';
+import useWindowSize from '../hooks/useWindowsSize';
 
 function App() {
 
+  const size = useWindowSize();
   const app = useRef();
   const scrollContainer = useRef();
 
   useEffect(() => {
     document.body.style.height = `${scrollContainer.current.getBoundingClientRect().height}px`;
-  }, []);
+  }, [size.height]);
 
   return (
     <div ref={app} className='App'>
